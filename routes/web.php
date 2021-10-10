d62454a16709365703c292c56f48e07168d81176
idanieldrew <danielgia35@gmail.com>
2021-10-10 10:30:44 +0330

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -2,68 +2,60 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// main page
+// landing page
 Route::get('/', 'LandingPageController@index')->name('land-page');
 
-// more products page
+// products page
 Route::get('/shop', 'ShopController@index')->name('shop-page');
 
-// one product with special id
+// single product
 Route::get('/shop/{id}', 'ShopController@show')->name('special-product');
 
-// cart page
-Route::get('/cart', 'CartController@index')->name('cart.index')->middleware('auth');
+Route::prefix('cart')->middleware('auth')->group(function () {
+    // cart page
+    Route::get('/cart', 'CartController@index')->name('cart.index');
 
-// submit a product
-Route::post('/cart', 'CartController@store')->name('cart-store')->middleware('auth');
+    // add product
+    Route::post('/cart', 'CartController@store')->name('cart-store');
+});
 
-// remove a product wwith rowId
+// remove a product with rowId
 Route::delete('/cart/{product}', 'CartController@destroy')->name('cart-destroy');
 
 // add save for later and add instance
 Route::post('/cart/switchToSaveForLater/{product}', 'CartController@switchToSaveForLater')->name('cart.SaveForLater');
 
-
+// remove a product in save for later with rowId
 Route::delete('/saveForLater/{product}', 'SaveForLaterController@destroy')->name('SaveForLater.destroy');
 
-
+// add product to save for later
 Route::post('/saveForLater/switchToCart/{product}', 'SaveForLaterController@switchToCart')->name('SaveForLater.switchToCart');
 
-
+// checkout page
 Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
 
+// submit stores in checkout
 Route::post('/checkout', 'CheckOutController@store')->name('checkout.store');
 
-
+// edit product was store
 Route::patch('/cart/{id}', 'CartController@update')->name('cart.update');
 
-
+// add coupon
 Route::post('/coupon', 'CouponController@store')->name('coupon.store');
 
-
+// remove coupon
 Route::delete('remove-coupon', 'CouponController@destroy')->name('coupon.destroy');
 
 // serch products
 Route::get('/search', 'shopController@search')->name('shop.search');
 
+// live search
 Route::get('/searches', 'shopController@searches')->name('shop.searches');
 
-
+// panel admin with voyager
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
+// auth with laravel ui
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
